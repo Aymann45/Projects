4c933678de2f42c90d4439ae0faaa920356e6661
Aymann45 <76143116+Aymann45@users.noreply.github.com>
2024-08-27 17:48:28 +0530

    Update Projects.py

--- Projects.py
+++ Projects.py
@@ -1,5 +1,5 @@
 print('Click Run to run the final project you will build')
 print('Welcome to Band Name Generator')
 city = input('What is the name of the city you grew up in?\n')
-# pet_name = input("What is your pet's name?\n")
+pet_name = input("What is your pet's name?\n")
 print('Your band name is: ' , city + " " + pet_name )
